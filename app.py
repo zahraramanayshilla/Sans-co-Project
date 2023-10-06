dc0f456f545a9640c57f0c6d2198fcd399e7f21a
zahraramanayshilla <zahraramanayshillasopian@gmail.com>
2023-10-06 20:16:09 +0700

    change

--- app.py
+++ app.py
@@ -1,23 +1,36 @@
+from flask import (Flask, redirect, url_for, render_template, request, jsonify)
 from pymongo import MongoClient
-import jwt
-from datetime import datetime, timedelta
-import hashlib
-from flask import (Flask,render_template,jsonify,request,redirect,url_for,session)
-
-from werkzeug.utils import secure_filename
+import requests
+from datetime import datetime
+from bson import ObjectId
 
 app = Flask(__name__)
-MONGODB_CONNECTION_STRING ='mongodb+srv://test:sparta@cluster0.jrmp0f9.mongodb.net/?retryWrites=true&w=majority'
-client = MongoClient (MONGODB_CONNECTION_STRING)
-db = client.dbsansco_projectpkl
-
-SECRET_KEY ='SPARTA'
 
 
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-# yang di uplod tersimpan disini
-app.config['UPLOAD_FOLDER'] = './static/profile_pics'
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def dashboard():
+    return render_template('about.html')
+
+@app.route('/menu')
+def edit():
+    return render_template('sheilamenu.html')
+
+@app.route('/gallery')
+def add():
+    return render_template('gallery.html')
+
+@app.route('/special-offers')
+def add():
+    return render_template('special-offers.html')
+
+
 
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
+
+
